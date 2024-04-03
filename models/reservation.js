5daa5c5749d82aca3713abbd4f9bf7a34d52245a
Noah Goff <noahgoff@Sidneys-MacBook-Pro.local>
2024-04-03 13:23:54 -0500

    final commit

--- models/reservation.js
+++ models/reservation.js
@@ -38,6 +38,34 @@ class Reservation {
 
     return results.rows.map(row => new Reservation(row));
   }
+
+  /*
+    Reservations Table
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    customer_id integer NOT NULL REFERENCES customers,
+    start_at timestamp without time zone NOT NULL,
+    num_guests integer NOT NULL,
+    notes text DEFAULT '' NOT NULL,
+    CONSTRAINT reservations_num_guests_check CHECK ((num_guests > 0))
+  */
+
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO reservations (customer_id, start_at, num_guests, notes)
+             VALUES ($1, $2, $3, $4)
+             RETURNING id`,
+        [this.customer_id, this.start_at, this.num_guests, this.notes]
+      );
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(
+        `UPDATE customers SET start_at=$1, num_guests=$2, notes=$3
+             WHERE id=$4`,
+        [this.start_at, this.num_guests, this.notes, this.id]
+      );
+    }
+  }
 }
 
 
